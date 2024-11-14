8cda6fda79bb88fd38bf2f416a80cc603dc30e59
jasmirchowdhury <jasmirchowdhury.123@gmail.com>
2024-11-14 22:52:53 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function myfunction(x,y)
+{
+    if (y=== undefined) {
+        y = 2;
+    }
+    document.write(x*y);
+}
+
+function addition(x,y)
+{
+    if (y=== undefined) {
+        y = 5;
+    }
+    document.write(x+y);
+}
+
